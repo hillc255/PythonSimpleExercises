8a1d7ad400fe344c5dc09bf468ea78fb0efe9c87
Claudia Hill <hillc255@yahoo.com>
2020-01-11 07:54:16 -0500

    pylint tests

---
+++ simple1.py
@@ -0,0 +1,17 @@
+'''
+A Very Simple Script - pylint check
+From Anaconda prompt in SimplePytonExercises - turn on pylint reports:  
+pylint -ry simple1.py
+
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
