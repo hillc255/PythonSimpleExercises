915d03498aadc3fddc98dfbf581acfcebd6b7b4b
Claudia Hill <hillc255@yahoo.com>
2020-01-12 08:01:40 -0500

    Add unit test example

---
+++ cap.py
@@ -0,0 +1,10 @@
+def cap_text(text):
+	'''
+	Input a string
+	Output the capitalized string
+	main program = cap.py
+	test program = test_cap.py
+	(base) C:...PythonSimpleExercises>python test_cap.py
+	'''
+	#return text.capitalize() - only first word string
+	return text.title()  #cap first letter in each word
